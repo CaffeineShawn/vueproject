346835700b115f27a57c967bdd15d537195f761a
Pcheih <976859652@qq.com>
2020-12-24 04:33:18 +0800

    install devDependencies
    通过npm安装各种loader，关键变化在config.js

--- vuewithoutcli/webpack.config.js
+++ vuewithoutcli/webpack.config.js
@@ -9,8 +9,38 @@ module.exports = {
   entry: './src/main.js',
   // 打包的出口
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  	filename: 'bundle.js',
+  	path: path.resolve(__dirname, 'dist')
   },
-  
+  // 打包规则
+  module: {
+  	rules: [
+  	{
+  		test: /\.vue$/,
+  		loader: 'vue-loader'
+  	},
+  	{
+  		test: /\.(jpg|jpeg|png|svg)$/,
+  		loader: 'file-loader',
+  		options: {
+  			name: '[name].[ext]'
+  		}
+  	},
+  	module: {
+  		rules: [{
+  			test: /\.css$/,
+  			use: ['style-loader', 'css-loader']
+  		}]
+  	},
+  	]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin()
+    ],
+    resolve: {
+    	alias: {
+    		'vue': 'vue/dist/vue.js'
+    	}
+    },
 }
