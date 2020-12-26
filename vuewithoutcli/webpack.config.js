05c16b94e92764679fa3ea7f5ea87ebe2a716480
Pcheih <976859652@qq.com>
2020-12-26 15:20:52 +0800

    Improve
    新增了Clean插件保持dist整洁
    更改了webpack打包模式为开发者模式

--- vuewithoutcli/webpack.config.js
+++ vuewithoutcli/webpack.config.js
@@ -5,6 +5,8 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 //引入 自动生成Html插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+//引入clean-webpack-plugin(提取出 CleanWebpackPlugin 的构造函数)
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
   // 打包的入口
   entry: './src/main.js',
@@ -42,7 +44,8 @@ module.exports = {
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template:'./index.html'
-    })
+    }),
+    new CleanWebpackPlugin()
     ],
     resolve: {
     	alias: {
