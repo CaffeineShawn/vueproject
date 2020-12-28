7bd8141afda92a13f2299719006b799833ef2c44
Pcheih <976859652@qq.com>
2020-12-28 15:02:44 +0800

    different config
    设置了不同情况下的打包配置
    并且知道了npm安装-D只是语法糖 帮助写入文件而已

---
+++ vuewithoutcli/webpack.prod.js
@@ -0,0 +1,67 @@
+// 使用node的path模块
+const path = require('path')
+
+// 引入vue-loader插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+//引入 自动生成Html插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//引入clean-webpack-plugin(提取出 CleanWebpackPlugin 的构造函数)
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//引入热模块替换
+const webpack = require('webpack')
+module.exports = {
+  mode: 'production',
+  // 打包的入口
+  entry: './src/main.js',
+  // 打包的出口
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer:{
+    contentBase:'./dist',
+    open:true,
+    hot:true
+  },
+  // 打包规则
+  module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    },
+    {
+      test: /\.(jpg|jpeg|png|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    },
+    {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+    },
+    {
+    test: /\.styl(us)?$/,
+    use: ['style-loader', 'css-loader', 'stylus-loader']
+  }
+    ]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template:'./index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+    ],
+
+    resolve: {
+      alias: {
+        'vue': 'vue/dist/vue.js'
+      }
+    },
+
+
+  }
