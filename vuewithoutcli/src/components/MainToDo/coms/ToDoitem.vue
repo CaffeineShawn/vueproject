af3aab18e4d9ce02f83cfdd045d229e36c771813
Pcheih <976859652@qq.com>
2021-01-02 14:54:49 +0800

    css done0
    完成了基本样式结构 主要技巧有：
    0：flex1进行拉伸
    1：伪类的使用
    2：过渡的使用
    3：划线、小手指等细节

--- vuewithoutcli/src/components/MainToDo/coms/ToDoitem.vue
+++ vuewithoutcli/src/components/MainToDo/coms/ToDoitem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="todo-item">
-    <input type="checkbox"/>
-    <label>todo1</label>
-    <button></button>
-  </div>
+	<div class="todo-item">
+		<input type="checkbox"/>
+		<label>todo1</label>
+		<button></button>
+	</div>
 </template>
 
 <script >
@@ -13,35 +13,56 @@
 </script>
 
 <style lang="stylus" type="text/css">
+@import '~styles/color.stylus'
 	.todo-item{
-		display: flex;
-		justify-content: space-between;
-		padding:10px;
+	display: flex;
+	justify-content: space-between;
+	padding:10px;
+	font-size: 24px;
+
+		&:hover{
+			button:before{
+				/*注：这里after和before相同 都是放在按钮中心*/
+				content: 'x'
+				font-size 24px
+				color $lightred
+			}
+		}
 	}
-		input{
-		appearance:none
+		&.completed{/*这里指选择自定义类 只要添加这个类就有效果*/
+			/*被划掉的样式*/
+			label{
+				color: #D9D9D9
+				text-decoration:line-through;
+			}
+		}
+	input{
+		/*appearance:none*/
 		width: 50px
 		height: 30px
 		border: none
 		outline: none
-		}
-	input::after{
-			content: url('images/ok.jpg');
-			width: 20px;
-			height: 20px;
-			overflow-wrap: hidden;
+		text-align:center;
 	}
-		
+	input:checked{
+		/*content: url('images/ok.jpg');*/
+	}
+
+
 	/*	&:checked:after
-			content: url('~/images/ok.jpg')	*/
+	content: url('~/images/ok.jpg')	*/
 	label{
 		flex: 1;
 		/*flex:1;指子元素自动伸缩 填充满整个元素 并且会根据其他的兄弟按flex：1、2、3比例进行伸缩 这里把todo内容拉到最长是合理的*/
+		transition:color 0.4s;
 	}
 	button{
 		width:40px;
 		background-color: transparent;
 		appearance:none;
-
+		border:none;
+		outline: none;
+		cursor: pointer;/*小手指*/
 	}
+
 </style>
